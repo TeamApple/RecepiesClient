10e33fd5322fbcfcd5105974e651b7aea9d49037
venelin-p-petrov <venelin.p.petrov@gmail.com>
2013-09-16 17:58:04 +0300

    done with recipe visualization

--- RecepiesClient/ViewModels/RecipesViewModel.cs
+++ RecepiesClient/ViewModels/RecipesViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Windows.Input;
 using RecepiesClient.Data;
+using RecepiesClient.Helpers;
 using RecepiesClient.Models;
 
 namespace RecepiesClient.ViewModels
@@ -12,9 +13,23 @@ namespace RecepiesClient.ViewModels
     public class RecipesViewModel : ViewModelBase, IPageViewModel
     {
         private string title;
+        private ICommand navigateToRecipe;
         private ObservableCollection<RecipeModel> recipes;
 
-        public ICommand NavigateToRecipeCommand { get; set; }
+        public ICommand NavigateToRecipeCommand
+        {
+            get
+            {
+                if (this.navigateToRecipe == null)
+                {
+                    this.navigateToRecipe = new RelayCommand(this.HandleNavigateToRecipeCommand);
+                }
+
+                return this.navigateToRecipe;
+            }
+        }
+
+        public event EventHandler<RecipeNavigateArgs> RecipeNavigate;
 
         public RecipeModel SelectedRecipe { get; set; }
 
@@ -25,12 +40,14 @@ namespace RecepiesClient.ViewModels
                 return;
             }
 
-            MessageBox.Show(SelectedRecipe.Name);
-        }
+            var newVM = new RecipeViewModel() { 
+                RecipeName = this.SelectedRecipe.Name,
+                Products = this.SelectedRecipe.Products,
+                CookingSteps = this.SelectedRecipe.CookingSteps,
+                ImagePath = this.SelectedRecipe.ImagePath
+            };
 
-        public RecipesViewModel()
-        {
-            this.NavigateToRecipeCommand = new RelayCommand(this.HandleNavigateToRecipeCommand);
+            this.RaiseRecipeNavigate(newVM);
         }
 
         public string Name
@@ -78,6 +95,14 @@ namespace RecepiesClient.ViewModels
                 {
                     this.recipes.Add(item);
                 }
+            }
+        }
+
+        protected void RaiseRecipeNavigate(RecipeViewModel vm)
+        {
+            if (this.RecipeNavigate != null)
+            {
+                this.RecipeNavigate(this, new RecipeNavigateArgs(vm));
             }
         }
     }
